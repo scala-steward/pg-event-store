d0b93fc5738f147bfd34c77ec21fa7344fb3f74a
Scala Steward <scala_steward@virtuslab.com>
2024-10-26 16:48:51 +0000

    Update logback-classic to 1.5.12

--- project/Libraries.scala
+++ project/Libraries.scala
@@ -31,7 +31,7 @@ object Libraries {
   )
 
   val logback = Seq(
-    "ch.qos.logback" % "logback-classic" % "1.5.11"
+    "ch.qos.logback" % "logback-classic" % "1.5.12"
   )
 
   val `play-json-libs` = Seq(
