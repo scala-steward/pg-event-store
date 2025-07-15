1684bc26d6f1ac1442c131fab521ec988826f94a
Scala Steward <scala_steward@virtuslab.com>
2025-07-15 16:10:30 +0000

    Update doobie-core, doobie-hikari, ... to 1.0.0-RC10

--- project/Libraries.scala
+++ project/Libraries.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object Libraries {
   val zio_version = "2.1.19"
-  val doobie_version = "1.0.0-RC9"
+  val doobie_version = "1.0.0-RC10"
 
   val zio = Seq(
     "dev.zio" %% "zio" % zio_version,
