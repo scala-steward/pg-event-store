8918cd5b16e57205fdf0c1c2f152c5e07c28f3ff
Scala Steward <scala_steward@virtuslab.com>
2025-12-23 00:07:20 +0000

    Update zio-interop-cats to 23.1.0.13

--- project/Libraries.scala
+++ project/Libraries.scala
@@ -20,7 +20,7 @@ object Libraries {
   )
 
   val doobie = Seq(
-    "dev.zio" %% "zio-interop-cats" % "23.1.0.11",
+    "dev.zio" %% "zio-interop-cats" % "23.1.0.13",
     "org.tpolecat" %% "doobie-core" % doobie_version,
     "org.tpolecat" %% "doobie-postgres" % doobie_version,
     "org.tpolecat" %% "doobie-hikari" % doobie_version
